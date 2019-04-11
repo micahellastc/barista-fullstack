d23b74be22acddd89035caa576f467095abb0bd6
DeFariasConsulting <defariasconsulting@gmail.com>
2019-04-11 12:40:09 -0400

    added database link and name

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 // config/database.js
 module.exports = {
-
-    'url' : 'mongodb+srv://roulette:roulette@cluster0-z4kgv.mongodb.net/roulette?retryWrites=true', 
-    'dbName': 'roulette'
+    'url' : 'mongodb+srv://demo:demo@barista-rv2tm.mongodb.net/merdaDeMacaco?retryWrites=true',
+    'dbName': 'merdaDeMacaco'
 };
